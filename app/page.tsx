447e2bb8484bf78555992b7fe0b4e824adc1cb7b
Gyan Jyoti Das <gyanjyotidas677@gmail.com>
2024-07-08 15:15:22 +0530

    feat: Add Footer component and update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -12,15 +13,18 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div  className="max-w-7xl w-full">
+       
+
         <FloatingNav
           navItems={navItems}
-        />
+          />
         
         <Hero/>
         <Grid/>
         <RecentProjects/>
         <Experience/>
         <Approach/>
+        <Footer/>
       </div>
     </main>
   );
